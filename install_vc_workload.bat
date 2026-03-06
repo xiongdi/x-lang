93e594c6b9047aad8423048c0dbab95b58ce5926
熊迪 <kotlin@qq.com>
2026-03-06 21:25:04 +0800

    删除根目录下的脚本文件

--- install_vc_workload.bat
+++
@@ -1,5 +0,0 @@
-
-@echo off
-echo Installing Desktop development with C++ workload...
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended --passive --wait
-echo Done! Please restart the build.
--- just_run.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-echo Running examples/hello.x...
-"C:\Users\Administrator\Documents\x-lang\tools\target\debug\x.exe" run "C:\Users\Administrator\Documents\x-lang\examples\hello.x"

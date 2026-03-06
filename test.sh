ad5a27570e1db5b96a88b62b002e6ff34437e84b
熊迪 <kotlin@qq.com>
2026-03-06 21:10:53 +0800

    删除临时测试文件和构建脚本

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Run all unit tests (requires LLVM for x-codegen; otherwise use:
-#   cargo test -p x-lexer -p x-parser -p x-typechecker -p x-hir -p x-perceus -p x-interpreter)
-cargo test
-echo "Running spec tests..."
-cargo run -p x-spec
-echo "All tests passed."

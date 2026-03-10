587fa58298c507676592229644dd8a0f3cedeba0
熊迪 <kotlin@qq.com>
2026-03-10 17:30:44 +0800

    修复Playground Wasm模块加载错误，添加导入断言

--- docs/playground/x_codegen_wasm.js
+++ docs/playground/x_codegen_wasm.js
@@ -1,6 +1,6 @@
 /* @ts-self-types="./x_codegen_wasm.d.ts" */
 
-import * as wasm from "./x_codegen_wasm_bg.wasm";
+import * as wasm from "./x_codegen_wasm_bg.wasm" assert { type: 'webassembly' };
 import { __wbg_set_wasm } from "./x_codegen_wasm_bg.js";
 __wbg_set_wasm(wasm);
 wasm.__wbindgen_start();
